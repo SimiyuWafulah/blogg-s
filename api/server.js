ca8efe676dcd15d5c280001d6ab525c12887131f
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-02-12 16:45:39 +0300

    connects to the database

--- api/server.js
+++ api/server.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to Database')
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(PORT, () => {
     console.log('Server is Running')
