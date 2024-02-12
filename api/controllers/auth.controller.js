08654f61aa931ad07662b9ddcbf0a221a2d1465f
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-02-13 00:31:06 +0300

    sign in api route complete

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     try {
@@ -15,6 +16,29 @@ export const signup = async (req, res, next) => {
         res
         .status(201)
         .json({message: 'Account created Successfuly'});
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    if (!email || !password || email==='' || password==='') {
+        next(errorHandler(401, 'All fields are required'))
+   }
+    try {
+        //verify user email
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+        //verify password
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(404,'Incorrect email or password'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT)
+        const {password: pass, ...rest} = validUser._doc
+        res
+        .cookie('access_token', token, {httpOnly: true})
+        .status(200)
+        .json(rest)
     } catch (error) {
         next(error)
     }
