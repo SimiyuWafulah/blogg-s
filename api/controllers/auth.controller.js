b23a5ca795c9b026324336a149d09e178af5e1e1
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-02-12 21:17:39 +0300

    middleware and a function to correctly handle all possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,16 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     try {
         const {username, email, password} = req.body;
         if(! username || email || password || username==='' || email==='' || password==='') {
-            return res.status(400).json({ message : 'All fileds are required'})
+            return next(errorHandler(401, 'All fields are required'))
         }
         //check if user exists
         const existingUser = await User.findOne({$or : [{email, password}]})
-        if(existingUser) return res.status(401).json({messsage:'Account already exists'});
+        if(existingUser) return next(errorHandler(409, 'Account already exists'))
         //register new user
         const hashPassword = await bcryptjs.hash(password, 10);
         const newUser = User({username, email, password: hashPassword});
@@ -18,6 +19,6 @@ export const signup = async (req, res) => {
         .status(201)
         .json({message: 'Account created Successfuly'});
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 }
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import userRoute from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
 import dotenv from 'dotenv';
+import { errorMiddleware } from './middlewares/error.middleware.js';
 dotenv.config();
 
 const app = express();
@@ -23,3 +24,5 @@ app.listen(PORT, () => {
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+
+app.use(errorMiddleware);
