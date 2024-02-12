a6cee104f2bc3d34f4251bb36e5ffce936a3936c
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-02-12 15:13:36 +0300

    creates pages and routes to the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div className = 'text-red-700'>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element ={<About/>}/>
+        <Route path='/signin' element = {<SignIn/>}/>
+        <Route path='/signup' element = {<SignUp/>}/>
+        <Route path='/dashboard' element = {<Dashboard/>}/>
+        <Route path='/projects' element ={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
