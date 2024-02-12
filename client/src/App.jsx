fcc0f31bdaf57925379d7b54857ef5a180c2a5a6
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-02-12 23:58:51 +0300

    footer component ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import FooterN from './components/FooterN'
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path='/dashboard' element = {<Dashboard/>}/>
         <Route path='/projects' element ={<Projects/>}/>
       </Routes>
+      <FooterN/>
     </BrowserRouter>
   )
 }
